65b9591c979f098ac2fbe43844c21c97a9094a87
gc <gc@ubuntu.ubuntu-domain>
2017-06-25 17:07:09 +0800

    bug xiufu

--- src/main/java/www/spring_boot/com/spring/boot/starter/App.java
+++ src/main/java/www/spring_boot/com/spring/boot/starter/App.java
@@ -1,7 +1,5 @@
 package www.spring_boot.com.spring.boot.starter;
 
-import com.test.TestA;
-
 /**
  * Hello world!
  *
@@ -10,8 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        TestA ta=new TestA();
-        ta.test();
+        System.out.println( "Hello World! bug1 bratch" );
     }
 }
