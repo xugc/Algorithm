4a8dd1f79405d42c4b400c5a88225f3919c5022e
xugc <guchengxu1988@163.com>
2017-06-26 10:47:52 +0800

    test

---
+++ src/main/java/test/Test.java
@@ -0,0 +1,32 @@
+/**
+ * @Title Test.java
+ * @Package test
+ * @Description TODO
+ * Copyright: Copyright (c) 2017 
+ * Company:*******
+ * 
+ * @author 徐故成
+ * @date 2017-6-26 上午10:47:16
+ * @version V1.0
+ */
+package test;
+
+/**  
+ * @ClassName Test  
+ * @Description TODO  
+ *    
+ */
+public class Test {
+
+	/**  
+	 * @Title main  
+	 * @Description TODO(这里用一句话描述这个方法的作用)  
+	 * @param args    设定文件  
+	 * @return void    返回类型  
+	 * @throws  
+	 */
+	public static void main(String[] args) {
+		System.out.println("master");
+	}
+
+}
